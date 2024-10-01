4f46d6b64b3bdaa8b3a7e555f815fe4e36d8b861
Anton Gorokhov <bonduelle85@gmail.com>
2024-10-01 21:41:12 +0300

    getMembersData

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,22 @@
+const url = "https://bonduelle85.github.io/wdd231/chamber/data/members.json";
+
+let members = document.querySelector("#business-members");
+
+async function getMembersData(view = "grid") {
+  let response = await fetch(url);
+  let data = await response.json();
+  let members = data.members;
+
+  switch (view) {
+    case "grid":
+      displayMembersGrid(members);
+      break;
+    case "list":
+      displayMembersList(members);
+      break;  
+    default:
+      break;
+  }
+
+  console.log(members);
+}
