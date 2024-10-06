39f3ad590da78c074c14aaab4f4763264d2d2e1d
Anton Gorokhov <bonduelle85@gmail.com>
2024-10-06 18:26:29 +0300

    displayForecast

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -77,27 +77,20 @@ hamButton.addEventListener("click", () => {
 });
 
 // Display type
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
 
-gridbutton.addEventListener("click", () => {
+gridButton.addEventListener("click", () => {
   memberCards.classList.add("grid");
   memberCards.classList.remove("list");
 });
 
-listbutton.addEventListener("click", () => {
+listButton.addEventListener("click", () => {
   memberCards.classList.add("list");
   memberCards.classList.remove("grid");
 });
 
 // Weather
-const weatherIcon = document.querySelector("#weather-icon");
-const currentTemp = document.querySelector("#current-temp");
-const currentDesc = document.querySelector("#current-desc");
-const maxTemp = document.querySelector("#temp-max");
-const minTemp = document.querySelector("#temp-min");
-const sunrise = document.querySelector("#sunrise");
-const sunset = document.querySelector("#sunset");
 const lat = 55.76;
 const lon = 37.62;
 const apiKey = "ad18ff83e7a5532d6104bf91da401a88";
@@ -110,9 +103,8 @@ async function getWeatherData() {
     const response = await fetch(forecastUrl);
     if (response.ok) {
       const data = await response.json();
-      console.log(data); // for testing json object
       displayWeather(data);
-      // displayForecast(data);
+      displayForecast(data);
     } else {
       throw Error(await response.text());
     }
@@ -122,14 +114,26 @@ async function getWeatherData() {
 }
 
 function displayWeather(data) {
+  let weatherIcon = document.querySelector("#weather-icon");
+  let currentTemp = document.querySelector("#current-temp");
+  let currentDesc = document.querySelector("#current-desc");
+  let maxTemp = document.querySelector("#temp-max");
+  let minTemp = document.querySelector("#temp-min");
+  let sunrise = document.querySelector("#sunrise");
+  let sunset = document.querySelector("#sunset");
+
   let iconSrc = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
   weatherIcon.setAttribute("src", iconSrc);
   weatherIcon.setAttribute("alt", `${data.list[0].weather[0].main}`);
 
-  currentTemp.innerHTML = `<p><b>${data.list[0].main.temp}</b>°C</p>`;
-  currentDesc.innerHTML = `${data.list[0].weather[0].description}`;
-  maxTemp.innerHTML = `<p>Max: ${data.list[0].main.temp_max}°C</p>`;
-  minTemp.innerHTML = `<p>Min: ${data.list[0].main.temp_min}°C</p>`;
+  currentTemp.innerHTML = `<p>Temp: <b>${Math.round(
+    data.list[0].main.temp
+  )}</b>°C</p>`;
+  currentDesc.innerHTML = capitalizeWords(
+    `${data.list[0].weather[0].description}`
+  );
+  maxTemp.innerHTML = `<p>Max: ${Math.round(data.list[0].main.temp_max)}°C</p>`;
+  minTemp.innerHTML = `<p>Min: ${Math.round(data.list[0].main.temp_min)}°C</p>`;
 
   let sunriseUnixTime = data.city.sunrise;
   let formattedSunriseTime = utsToLocalTime(sunriseUnixTime);
@@ -139,14 +143,56 @@ function displayWeather(data) {
   let formattedSunsetTime = utsToLocalTime(sunsetUnixTime);
   sunset.innerHTML = `Sunset: ${formattedSunsetTime}`;
 }
-function displayForecast(data) {}
+function displayForecast(data) {
+  // 8 timestamps per day -> FIRST_DAY = 0, SECOND_DAY = 8, THIRD_DAY = 16
+  let FIRST_DAY = 0;
+  let SECOND_DAY = 8;
+  let THIRD_DAY = 16;
+  let firstDay = document.querySelector("#first-day");
+  let secondDay = document.querySelector("#second-day");
+  let thirdDay = document.querySelector("#third-day");
 
+  let firstDayName = utsToLocalDay(data.list[FIRST_DAY].dt);
+  firstDay.innerHTML = `<p>${firstDayName}: <b>${Math.round(
+    data.list[FIRST_DAY].main.temp
+  )}</b>°C</p>`;
+  let secondDayName = utsToLocalDay(data.list[SECOND_DAY].dt);
+  secondDay.innerHTML = `<p>${secondDayName}: <b>${Math.round(
+    data.list[SECOND_DAY].main.temp
+  )}</b>°C</p>`;
+  let thirdDayName = utsToLocalDay(data.list[THIRD_DAY].dt);
+  thirdDay.innerHTML = `<p>${thirdDayName}: <b>${Math.round(
+    data.list[THIRD_DAY].main.temp
+  )}</b>°C</p>`;
+}
+
+function capitalizeWords(str) {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
 function utsToLocalTime(utsTime) {
   let unixTimeMillis = utsTime * 1000;
   let date = new Date(unixTimeMillis);
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const hours = String(date.getHours());
+  const minutes = String(date.getMinutes());
   return `${hours}:${minutes}`;
+}
+function utsToLocalDay(utsTime) {
+  let unixTimeMillis = utsTime * 1000;
+  let date = new Date(unixTimeMillis);
+  let dayOfWeekNumbers = date.getUTCDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[dayOfWeekNumbers];
 }
 
 // Footer
