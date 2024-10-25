b7254bac9b54f2b1bcc9f5491ae705a5de693301
Anton Gorokhov <bonduelle85@gmail.com>
2024-10-25 15:24:43 +0300

    currency img links

--- glide-and-ride/scripts/currency.mjs
+++ glide-and-ride/scripts/currency.mjs
@@ -5,38 +5,78 @@ const GBP = "GBP";
 const EUR = "EUR";
 const CNY = "CNY";
 
+let usdRate;
+let eurRate;
+let gbpRate;
+let cnyRate;
+
+fetchAllCurrencies();
+
+async function fetchAllCurrencies() {
+    await Promise.all([
+      getCurrencyData(USD),
+      getCurrencyData(GBP),
+      getCurrencyData(EUR),
+      getCurrencyData(CNY)
+    ]);
+    displayCurrency();
+  }
+
 async function getCurrencyData(currency) {
-  const url = ` https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currency}`;    
-  let response = await fetch(url);
-  let json = await response.json();
-  let rubles = json.conversion_rates.RUB;
-  return rubles
+    // let url = ` https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currency}`;
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      let json = await response.json();
+      let data = json.conversion_rates.RUB;
+      switch (currency) {
+        case "USD":
+          usdRate = data;
+          break;
+        case "GBP":
+          gbpRate = data;
+          break;
+        case "EUR":
+          eurRate = data;
+          break;
+        case "CNY":
+          cnyRate = data;
+          break;
+        default:
+          break;
+      }
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function displayCurrency() {
-    document.querySelector(".exchange-rate-card").innerHTML = `
+  document.querySelector(".exchange-rate-card").innerHTML = `
         <h3>Exchange Rate</h3><hr>
         <p>
-            <img src="../images/currency/us.svg" width="28" height="28" alt="USA flag" loading="lazy"/>
-            1$ USD = <span>${getCurrencyData(USD)}</span>₽ RUB
-            <img src="../images/currency/ru.svg" width="28" height="28" alt="RU flag" loading="lazy"/>
+            <img src="https://bonduelle85.github.io/wdd231/glide-and-ride/images/currency/us.svg" width="28" height="28" alt="USA flag" loading="lazy"/>
+            1$  = <span>${Math.ceil(usdRate)}</span>₽ 
+            <img src="https://bonduelle85.github.io/wdd231/glide-and-ride/images/currency/ru.svg" width="28" height="28" alt="RU flag" loading="lazy"/>
         </p>
         <p>
-            <img src="../images/currency/eu.svg" width="28" height="28" alt="EU flag" loading="lazy"/>
-            1€ EUR = <span>${getCurrencyData(EUR)}</span>₽ RUB
-            <img src="../images/currency/ru.svg" width="28" height="28" alt="RU flag" loading="lazy"/>
+            <img src="https://bonduelle85.github.io/wdd231/glide-and-ride/images/currency/eu.svg" width="28" height="28" alt="EU flag" loading="lazy"/>
+            1€  = <span>${Math.ceil(eurRate)}</span>₽ 
+            <img src="https://bonduelle85.github.io/wdd231/glide-and-ride/images/currency/ru.svg" width="28" height="28" alt="RU flag" loading="lazy"/>
         </p>
         <p>
-            <img src="../images/currency/gb.svg" width="28" height="28" alt="GB flag" loading="lazy"/>
-            1£ GBP = <span>${getCurrencyData(GBP)}</span>₽ RUB
-            <img src="../images/currency/ru.svg" width="28" height="28" alt="RU flag" loading="lazy"/>
+            <img src="https://bonduelle85.github.io/wdd231/glide-and-ride/images/currency/gb.svg" width="28" height="28" alt="GB flag" loading="lazy"/>
+            1£  = <span>${Math.ceil(gbpRate)}</span>₽ 
+            <img src="https://bonduelle85.github.io/wdd231/glide-and-ride/images/currency/ru.svg" width="28" height="28" alt="RU flag" loading="lazy"/>
         </p>
         <p>
-            <img src="../images/currency/cn.svg" width="28" height="28" alt="CN flag" loading="lazy"/>
-            1¥ CNY = <span>${getCurrencyData(CNY)}</span>₽ RUB
-            <img src="../images/currency/ru.svg" width="28" height="28" alt="RU flag" loading="lazy"/>
+            <img src="https://bonduelle85.github.io/wdd231/glide-and-ride/images/currency/cn.svg" width="28" height="28" alt="CN flag" loading="lazy"/>
+            1¥  = <span>${Math.ceil(cnyRate)}</span>₽ 
+            <img src="https://bonduelle85.github.io/wdd231/glide-and-ride/images/currency/ru.svg" width="28" height="28" alt="RU flag" loading="lazy"/>
         </p>
-    `
+    `;
 }
 
 export default displayCurrency;
